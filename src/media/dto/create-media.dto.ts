17be9c6905bbdd2b5c3d5cd64123e0b838806ed7
Kevin Poveda <kevinpovedaj@gmail.com>
2026-09-18 10:21:36 -0500

    feat: implement CreateMediaDto and update MediaService with uploadMedia method

---
+++ src/media/dto/create-media.dto.ts
@@ -0,0 +1,19 @@
+import { IsEnum, IsPositive, IsString, IsUUID, Max, Min } from "class-validator"
+import { MediaPurposeEnum } from "../enums/media-purpose.enum"
+
+
+export class CreateMediaDto {
+
+    @IsUUID()
+    owner: string
+    @IsEnum(MediaPurposeEnum)
+    purpose: MediaPurposeEnum
+
+    @IsString()
+    contentType: string
+
+    @IsPositive()
+    @Min(0)
+    @Max(1000000000)
+    size: number
+}
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -3,21 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MediaService {
 
-  uploadMedia() { }
+  uploadMedia() {
 
-  findAll() {
-    return `This action returns all media`;
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} media`;
-  }
 
-  update(id: number) {
-    return `This action updates a #${id} media`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} media`;
-  }
+
 }
